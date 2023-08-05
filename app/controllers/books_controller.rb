2b4496acc70244e68d186c625eb6ed956c0ac1dc
EC2 Default User <ec2-user@ip-172-31-46-53.ap-northeast-1.compute.internal>
2023-08-05 05:13:18 +0000

    課題2[destroy途中]

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,7 +10,7 @@ class BooksController < ApplicationController
     
     book = Book.new(book_params)
     book.save
-    redirect_to '/books/#{book.id}'
+    redirect_to book_path(book.id)
     
   end
   
@@ -20,6 +20,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to '/books'
   end
   
   private
